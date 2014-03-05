573acaecf5af8365cdf7b13bf701872e56506fdb
abu <justabu@gmail.com>
2014-03-05 18:17:56 +0530

    route for creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Spree::Core::Engine.routes.append do
-  match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:post], :as => :gateway_payuin_callback
+  #match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:post], :as => :gateway_payuin_callback
+  match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:get,:post]
 end
