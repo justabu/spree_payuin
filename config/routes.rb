6737308eac0895a78feef7059e11463c88be7767
abu <justabu@gmail.com>
2014-03-05 19:03:10 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Spree::Core::Engine.routes.append do
-  match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:post], :as => :gateway_payuin_callback
-  #match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:get,:post]
+  match "checkout/gateway/payuin/callback" => 'checkout#callback', :via => [:post], :as => :gateway_payuin_callback, , :only [:create]
 end
